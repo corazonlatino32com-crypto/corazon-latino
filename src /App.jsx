6677b09174bb712884d4426b272451d855737550
corazonlatino32com-crypto <corazonlatino32.com@gmail.com>
2026-04-11 17:05:19 +0200

    Delete src  directory

--- src /App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ fontFamily: "sans-serif", padding: 40 }}>
-      <h1>Corazón Latino</h1>
-      <p>Tienda funcionando ✔️</p>
-    </div>
-  );
-}
