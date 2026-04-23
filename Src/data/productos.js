b706d7132b0471c3ea490fc726e9f0299315e6db
corazonlatino32com-crypto <corazonlatino32.com@gmail.com>
2026-04-23 13:26:16 +0200

    Update productos.js

--- Src/data/productos.js
+++ Src/data/productos.js
@@ -1,16 +1,17 @@
+
 export const products = [
   {
     id: "wrap-01",
     name: "Caribeña Silk Wrap",
     price: 25,
-    image: "/wrap.jpg",
-    description: "Protección para cabello afro con estilo."
+    image: "https://via.placeholder.com/150",
+    description: "Protección para cabello afro."
   },
   {
     id: "tee-01",
     name: "Afro Caribe Tee",
     price: 35,
-    image: "/shirt.jpg",
-    description: "Camiseta con identidad caribeña."
+    image: "https://via.placeholder.com/150",
+    description: "Camiseta con identidad."
   }
 ];
